71e08e4da9d8a22d9cf368251d202842bda0c11e
dnalvng <122769557+dnalvng@users.noreply.github.com>
2023-02-16 15:01:42 -0500

    Add files via upload

---
+++ deliverable5/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace deliverable5;
+class Program
+{
+    static void Main(string[] args)
+
+    {
+        int[] numbers = new int[10];
+        Random rand = new Random();
+
+        for (int i = 0; i <numbers.Length; i++)
+        {
+            numbers[i] = rand.Next(10, 51);
+        }
+
+        Console.WriteLine("Randomly generated numbers:");
+        foreach(int num in numbers)
+        {
+            Console.Write(num + "");
+        }
+        Console.WriteLine();
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        Console.WriteLine("Sum of all elements in the array: " + sum);
+
+    }
+
+}
+
